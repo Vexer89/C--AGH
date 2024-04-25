c7ba213cd26af78048b73a796a09e4f4fe54a9c8
FerdKrzy <115992139+Vexer89@users.noreply.github.com>
2024-04-25 19:01:40 +0200

    Add files via upload

---
+++ LAB05/LAB05/Producer-Consumer-Problem/Ex1.cs
@@ -0,0 +1,72 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB05
+{
+    public class Ex1
+    {
+        Random random = new Random();
+        List<Producer> producers = new List<Producer>();
+        List<Consumer> consumers = new List<Consumer>();
+        List<int> data = new List<int>();
+        int m;
+        int n;
+
+        public Ex1(int m_, int n_)
+        {
+            this.m = m_;
+            this.n = n_;
+        }
+
+        public void Start()
+        {
+            for (int i =  0; i < m; i++)
+            {
+                Consumer consumer = new Consumer(i, data, random.Next(100, 1000));
+                consumer.Thread = new Thread(new ThreadStart(consumer.Start));
+                consumers.Add(consumer);
+            }
+            for (int i =  0; i < n; i++)
+            {
+                Producer producer = new Producer(i, data, random.Next(200, 2000));
+                producer.Thread = new Thread(new ThreadStart(producer.Start));
+                producers.Add(producer);
+            }
+            foreach ( var i in producers)
+            {
+                i.Thread.Start();
+                Console.WriteLine("Producer " + i.Number + " started");
+            }
+            foreach ( var i in consumers)
+            {
+                i.Thread.Start();
+                Console.WriteLine("Consumer " + i.Number + " started");
+            }
+            
+            Console.WriteLine("Press 'q' to quit.");
+            while (Console.ReadKey().Key != ConsoleKey.Q)
+            {
+                Console.WriteLine("Press 'q' to quit.");
+            }
+            Stop();
+        }
+
+        public void Stop()
+        {
+            foreach (var i in producers)
+            {
+                i.Running = false;
+                Console.WriteLine("Producer " + i.Number + " stopped");
+            }
+            foreach (var i in consumers)
+            {
+                i.Running = false;
+                Console.WriteLine("Consumer " + i.Number + " stopped");
+            }
+        }
+    }
+}
+
---
+++ LAB05/LAB05/Producer-Consumer-Problem/ProducerConsumer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace LAB05
+{
+    public class ProducerConsumer
+    {
+        public int Number { get; set; }
+        public List<int> Data { get; set; }
+        public Thread Thread = null;
+        public int TimeSleep { get; set; }
+
+        public ProducerConsumer(int number, List<int> data, int timeSleep)
+        {
+            Number = number;
+            Data = data;
+            TimeSleep = timeSleep;
+        }
+    }
+
+    public class Producer : ProducerConsumer
+    {
+        public bool Running { get; set; }
+
+        public Producer(int number, List<int> data, int timeSleep) : base(number, data, timeSleep)
+        {
+            Running = true;
+        }
+
+        public void Start()
+        {
+            while (Running)
+            {
+                Thread.Sleep(TimeSleep);
+                WriteData();
+            }
+        }
+
+        public void WriteData()
+        {
+            lock (Data)
+            {
+                Data.Add(Number);
+            }
+        }
+    }
+
+    public class Consumer : ProducerConsumer
+    {
+        public List<int> Consumed { get; set; }
+        public Dictionary<int, int> ConsumedFromEachProducer { get; set; }
+        public bool Running { get; set; }
+
+        public Consumer(int number, List<int> data, int timeSleep) : base(number, data, timeSleep)
+        {
+            ConsumedFromEachProducer = new Dictionary<int, int>();
+            Running = true;
+        }
+
+        public void Start()
+        {
+            while (Running)
+            {
+                Thread.Sleep(TimeSleep);
+                GetData();
+            }
+            PrintConsumedData();
+        }
+
+        public void GetData()
+        {
+            lock (Data)
+            {
+                if (Data.Count == 0) return;
+                int lastItem = Data[Data.Count - 1];
+                if (ConsumedFromEachProducer.ContainsKey(lastItem))
+                {
+                    ConsumedFromEachProducer[lastItem]++;
+                }
+                else
+                {
+                    ConsumedFromEachProducer[lastItem] = 1;
+                }
+                Data.RemoveAt(Data.Count - 1);
+            }
+        }
+        
+        public void PrintConsumedData()
+        {
+            Console.WriteLine($"Consumer {Number} consumed data:");
+            foreach (var i in ConsumedFromEachProducer)
+            {
+                Console.WriteLine($"Producer {i.Key} - {i.Value}");
+            }
+        }
+    }
+}
